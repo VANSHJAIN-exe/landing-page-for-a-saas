3545b6d841f76e09c6a669988af3b2bafb4cbf4e
Vansh Jain <vanshjain847@gmail.com>
2025-04-06 02:58:12 +0530

    Update contact link in header to 'Contact us'

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact us</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
